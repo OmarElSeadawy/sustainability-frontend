0bcefccf7eb6ff2497aa26518b87f9b851a49669
mohamedashraftaha <mohammedashraf@aucegypt.edu>
2024-02-12 17:46:23 +0200

    [+] Backend Added

--- backend/run.sh
+++ backend/run.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-pip3 install debugpy
-
-export FLASK_APP=run
-python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m flask run -h 0.0.0.0 -p 5000
-
-
-# gunicorn -w 1 --bind 0.0.0.0:5000 run:app --graceful-timeout 60 --keep-alive 150 --backlog 2048 --timeout 60
+gunicorn -w 1 --bind 0.0.0.0:5000 run:app --graceful-timeout 60 --keep-alive 150 --backlog 2048 --timeout 60
