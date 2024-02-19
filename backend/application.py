c3e54e00c3688dd5741e1924e4a6b893736a547e
mohamedashraftaha <mohammedashraf@aucegypt.edu>
2024-02-20 00:15:29 +0200

    [+] CORS Handling

--- backend/application.py
+++ backend/application.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_restful import Api
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
@@ -37,6 +38,7 @@ migrate = Migrate(app, db, compare_type=True)
 
 from apis import Login, Register, CreateSurvey, GetSurvey
 
+CORS(app)
 bcrypt = Bcrypt(app)
 babel = Babel(app)
 bp_api = Blueprint("api", __name__)
